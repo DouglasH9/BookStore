3b30ff30688ce0d0449879eced4fd114288b1567
DouglasH9 <douglash9882@gmail.com>
2023-01-23 13:52:07 -0600

    asdf

---
+++ BookStoreWebApp/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BookStoreWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWebApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
+
